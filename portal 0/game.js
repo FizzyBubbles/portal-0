aa05d5fdeeede995653f0aea71bb5f28afac4277
daddybiggboii <41658849+daddybiggboii@users.noreply.github.com>
2018-07-25 18:29:16 +1000

    Update game.js

--- portal 0/game.js
+++ portal 0/game.js
@@ -1,23 +1,34 @@
 var char;
 
 function setup() {
-    createCanvas(500, 500);
-    char = new player(width / 2, height / 2);
+   createCanvas(500, 500);
+   char = new player(width / 2, height / 2);
 }
 
 function draw() {
-    background(0);
-    char.nOne += char.nInc;
-    char.nTwo += char.nInc;
-
-    if (mouseIsPressed) {
-        char.tpX = mouseX;
-        char.tpY = mouseY;
-        char.RTT = true;
-    }
-    char.control();
-    char.collisionBox();
-    char.update();
-    char.show();
-
+  background(0);
+  char.nOne += char.nInc;
+  char.nTwo += char.nInc;
+	char.distChar += char.fillRate;
+	if (mouseIsPressed) {
+		//tries to find the mid point
+		var y = mouseY - char.y; 
+		var x = mouseX - char.x;
+		vara length = Math.sqrt((y * y) + (x * x));
+		if (length <= char.distChar) {
+			char.tpX = mouseX;
+			char.tpY = mouseY;
+		} else {
+			char.tpX = ((char.distChar / length) * (mouseX - char.x));
+			char.tpY = ((char.distChar / length) * (mouseY - char.y));
+		}
+		console.log((char.distChar / length) * (mouseX - char.x));
+		console.log((char.distChar / length) * (mouseY - char.y));
+		char.RTT = true;
+  }
+  char.control();
+  char.collisionBox();
+  char.update();
+  char.show();
+	
 }
